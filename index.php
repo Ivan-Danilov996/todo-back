edba0b4b90cffa28f2c44badcede310395c016cd
Ivan-Danilov996 <ivandanilov996@yandex.ru>
2021-03-26 16:20:10 +0300

    edit

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Credentials: true');
-echo('hello');
 $connect = mysqli_connect('remotemysql.com', 'LXoH1FNrh3', 'xUWrAdKnKX', 'LXoH1FNrh3');
 
 $method = $_SERVER['REQUEST_METHOD'];
