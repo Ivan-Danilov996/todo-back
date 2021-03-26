7017af7a806424103a322d1eee160c2a1e4579b3
Ivan-Danilov996 <ivandanilov996@yandex.ru>
2021-03-26 13:55:13 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Credentials: true');
+
+$connect = mysqli_connect('127.0.0.1', 'root', '', 'todo');
+
+$method = $_SERVER['REQUEST_METHOD'];
+$q = $_GET['q'];
+$params = explode('/', $q);
+$type = $params[0];
+
+function get_tasks()
+{
+    $task = [];
+    global $connect;
+    $tasks = mysqli_query($connect, "SELECT * FROM `tasks`");
+
+
+    while ($task_element = mysqli_fetch_assoc($tasks)) {
+        $task[] = $task_element;
+    }
+    return $task;
+}
+
+
+function sort_up($new_tasks)
+{
+    global $offset;
+    global $data;
+    $offset = $_GET['offset'];
+    $data = $_GET['sort'];
+    usort($new_tasks, function ($a, $b) {
+        global $data;
+        return strcmp((string) $a[$data], (string) $b[$data]);
+    });
+}
+
+function sort_down($new_tasks)
+{
+    global $offset;
+    global $data;
+    $offset = $_GET['offset'];
+    $data = $_GET['sort'];
+    usort($new_tasks, function ($a, $b) {
+        global $data;
+        return strcmp((string) $a[$data], (string) $b[$data]);
+    });
+}
+
+
+
+
+if ($method === 'GET') {
+    $new_tasks = get_tasks();
+    if ($type === 'tasks') {
+        echo json_encode(['task' => array_slice($new_tasks, 0, 3), 'length' => count($new_tasks)]);
+    } else if ($type === 'offset') {
+        $offset = $_GET['offset'];
+        echo json_encode(['task' => array_slice($new_tasks, $offset, 3), 'length' => count($new_tasks)]);
+    } else if ($type === 'sortup') {
+        sort_up($new_tasks);
+        echo json_encode(['task' => array_slice($new_tasks, $offset, 3), 'length' => count($new_tasks)]);
+    } else if ($type === 'sortdown') {
+        sort_down($new_tasks);
+        echo json_encode(['task' => array_slice($new_tasks, $offset, 3), 'length' => count($new_tasks)]);
+    }
+} else if ($method === 'POST') {
+    if ($type === 'tasks') {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $text = $_POST["textarea"];
+        $status = 0;
+        mysqli_query($connect, "INSERT INTO `tasks` (`name`,`email`,`text`,`status`) VALUES ('$name','$email','$text', $status)");
+        $new_tasks = get_tasks();
+        echo json_encode(['task' => array_slice($new_tasks, 0, 3), 'length' => count($new_tasks)]);
+    } else if ($type === 'auth') {
+        $login = $_POST["login"];
+        $password = $_POST["password"];
+        $password = md5($password);
+        $result = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
+        if (mysqli_num_rows($result) === 1) {
+            echo json_encode(['success' => 'ok']);
+        } else {
+            echo json_encode(['success' => 'error']);
+        }
+    }
+} else if ($method === 'PATCH') {
+    $id = $_GET['id'];
+    mysqli_query($connect, "UPDATE `tasks` SET status = 1 WHERE id = '$id'");
+    $new_tasks = get_tasks();
+    if ($type === 'offset') {
+        $offset = $_GET['offset'];
+        echo json_encode(['task' => array_slice($new_tasks, $offset, 3), 'length' => count($new_tasks)]);
+    } else if ($type === 'sortup') {
+        sort_up($new_tasks);
+        echo json_encode(['task' => array_slice($new_tasks, $offset, 3), 'length' => count($new_tasks)]);
+    } else if ($type === 'sortdown') {
+        sort_down($new_tasks);
+        echo json_encode(['task' => array_slice($new_tasks, $offset, 3), 'length' => count($new_tasks)]);
+    }
+}
---
+++ todo.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Мар 26 2021 г., 11:37
+-- Версия сервера: 10.4.18-MariaDB
+-- Версия PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `todo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `text` varchar(150) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `login` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(32) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`) VALUES
+(3, 'admin', '202cb962ac59075b964b07152d234b70');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
