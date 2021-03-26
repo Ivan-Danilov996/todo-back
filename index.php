c8c58655618c74ba444158dfc4433f3ca26fc0ed
Ivan-Danilov996 <ivandanilov996@yandex.ru>
2021-03-26 16:04:05 +0300

    index edit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Credentials: true');
-
+echo('hello');
 $connect = mysqli_connect('127.0.0.1', 'root', '', 'todo');
 
 $method = $_SERVER['REQUEST_METHOD'];
