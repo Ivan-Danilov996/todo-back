183856b0eb044bd2ab13f7c836817a3da7475872
Ivan-Danilov996 <ivandanilov996@yandex.ru>
2021-03-26 16:18:46 +0300

    edit connect

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Credentials: true');
 echo('hello');
-$connect = mysqli_connect('127.0.0.1', 'root', '', 'todo');
+$connect = mysqli_connect('remotemysql.com', 'LXoH1FNrh3', 'xUWrAdKnKX', 'LXoH1FNrh3');
 
 $method = $_SERVER['REQUEST_METHOD'];
 $q = $_GET['q'];
